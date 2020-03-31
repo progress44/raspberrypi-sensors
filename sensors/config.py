b35bc55d631d8f0cecf2f15143fbd84fed8bb86c
Ani <sinanajani@outlook.com>
2020-03-31 21:31:25 +0200

    [+] Fixed config file path

--- sensors/config.py
+++ sensors/config.py
@@ -1,10 +1,12 @@
 from yaml import load
+import os 
 
 class Config(object):
 	cfg = None
 
 	def __init__(self, file = "config.yml"):
-		self.config(file)
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		self.config(dir_path + file)
 
 	def config(self, file):
 		try:
