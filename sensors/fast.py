42648988ed19e18e9b7b5a6d344499bf0a9d031f
Ani <sinanajani@outlook.com>
2020-03-31 21:09:57 +0200

    [+] Added getopt

--- sensors/fast.py
+++ sensors/fast.py
@@ -9,6 +9,7 @@ from config import Config
 cfg = Config().get()
 logger = Log(cfg["main"]["log_file"]).get()
 loop = asyncio.get_event_loop()
+process = 0
 
 def signal_handler(signal, frame):
     loop.stop()
@@ -23,13 +24,32 @@ async def runner():
     asyncio.ensure_future(runner())
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hfs", ["help", "fast", "slow"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
 
-    # opts = getopt.getopt(argv, ["hfs"])
-    # print(opts)
-    
-    asyncio.ensure_future(runner())
-    loop.run_forever()
+    global process
+
+    for o, a in opts:
+        if o == "-f":
+            process = 1
+        elif o in ("-s"):
+            process = 2
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            assert False, "unhandled option"
+
+    print(process)
+
+    # signal.signal(signal.SIGINT, signal_handler)
+    # asyncio.ensure_future(runner())
+    # loop.run_forever()
 
 if __name__ == "__main__":
     main()
