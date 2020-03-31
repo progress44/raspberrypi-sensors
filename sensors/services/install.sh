290177759805bba7050732cd59d2acd86fadd3c9
Ani <sinanajani@outlook.com>
2020-03-31 21:46:41 +0200

    [+] Updated install script

--- sensors/services/install.sh
+++ sensors/services/install.sh
@@ -5,3 +5,8 @@ cp ./*.service /etc/systemd/system/
 
 systemctl enable --now fast-sensors
 systemctl enable --now slow-sensors
+
+systemctl daemon-reload
+
+systemctl restart fast-sensors
+systemctl restart slow-sensors
