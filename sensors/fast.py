34f2a112258a2b4fbd2784f6a4b0a7313de841c1
Ani <sinanajani@outlook.com>
2020-03-31 19:44:55 +0200

    [~] Refactor config and log

--- sensors/fast.py
+++ sensors/fast.py
@@ -1,52 +1,16 @@
 #!/usr/bin/env python
 
-import sys, signal, asyncio, time, logging, json, track
+import sys, signal, asyncio, time, logging, json, track, getopt
 from yaml import load
-from bosch import Bosch
-from enviro import Enviro
 from mapping import Mapping
 from track import Track
+from log import Log
+from config import Config
 
 cfg = None
 logger = None
 pid = None
-bosch = None
 loop = asyncio.get_event_loop()
-
-def config():
-    global cfg
-    global pid
-
-    try:
-        from yaml import CLoader as Loader
-    except ImportError:
-        from yaml import Loader
-
-    with open("config.yml", "r") as ymlfile:
-        cfg = load(ymlfile, Loader=Loader)
-    
-    pid = cfg["main"]["pid"]
-
-def logger():
-    global logger
-    global keep_fds
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    logger.propagate = False
-    fh = logging.FileHandler(cfg["main"]["log_file"], "w")
-    fh.setLevel(logging.DEBUG)
-    logger.addHandler(fh)
-    keep_fds = [fh.stream.fileno()]
-
-async def trackFast():
-    final = await Mapping().fastSensors()
-    logger.debug(json.dumps(final))
-
-    # make server request
-    r = await Track().event(final)
-
-
 
 def signal_handler(signal, frame):
     loop.stop()
@@ -55,19 +19,20 @@ def signal_handler(signal, frame):
 async def runner():
     time.sleep(cfg["main"]["interval"])
 
-    await trackFast()
+    await Mapping().trackFast()
+    await Mapping().trackSlow()
+
     asyncio.ensure_future(runner())
 
-    return None
-
 def main():
-    global bosch
-
     signal.signal(signal.SIGINT, signal_handler)
     
-    bosch = Bosch()
-    config()
-    logger()
+    cfg = Config().get()
+    logger = Log(cfg["main"]["log_file"]).get()
+
+    # opts = getopt.getopt(argv, ["hfs"])
+
+    # print(opts)
     
     asyncio.ensure_future(runner())
     loop.run_forever()
