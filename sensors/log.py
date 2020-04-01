a50c00e0ea51458964568611ab7f90fe2d87d43b
Ani <sinanajani@outlook.com>
2020-04-01 22:47:57 +0200

    [x] Loading only not loaded files

--- sensors/log.py
+++ sensors/log.py
@@ -1,22 +1,26 @@
 import logging, os
 
 class Log(object):
-	
+	loadedFiles = []
+
 	def __init__(self, file):
 		dir_path = os.path.dirname(os.path.realpath(__file__)) +"/"
 		self.logger_setup(dir_path + file)
 		self.logger = logging.getLogger(__name__)
 
 	def logger_setup(self, file):
-		if self.fh != None or self.keep_fds != None:
-			return
-			
+		for loaded in Log.loadedFiles:
+			if loaded == file:
+				return None
+
+		Log.loadedFiles = Log.loadedFiles + [file]
+		
 		self.logger.setLevel(logging.DEBUG)
 		self.logger.propagate = False
-		self.fh = logging.FileHandler(file, "w")
-		self.fh.setLevel(logging.DEBUG)
-		self.logger.addHandler(self.fh)
-		self.keep_fds = [self.fh.stream.fileno()]
+		fh = logging.FileHandler(file, "w")
+		fh.setLevel(logging.DEBUG)
+		self.logger.addHandler(fh)
+		self.keep_fds = [fh.stream.fileno()]
 	
 	def get(self):
 		return self.logger
